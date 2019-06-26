694f909f7f4880e6604251e333fcb67e2d03bab0
Jinwook Jeong <vustthat@gmail.com>
2019-06-27 02:34:09 +0900

    Codeforces 1183 - C
    downcasting error...

--- codeforces/1183/C.cc
+++ codeforces/1183/C.cc
@@ -3,7 +3,7 @@ using namespace std;
 int query() {
   int k,n,a,b;
   scanf("%d%d%d%d",&k,&n,&a,&b);
-  int ret = ((k - 1LL*b*n) + (a-b-1)) / (a-b) - 1;
+  long long ret = (k-1LL*b*n + (a-b-1)) / (a-b) - 1;
   if(ret > n) return n;
   if(ret < 0) return -1;
   return ret;
